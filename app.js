188537593e792282a911be756416c2a808f55740
amazingawwal <sakeebmawwal@gmail.com>
2025-06-12 23:59:56 +0100

    still on task 5

--- app.js
+++ app.js
@@ -80,13 +80,46 @@ const s4counter1 = s4createCounter(5);
 const s4counter2 = s4createCounter(12);
 
 // 1st counter instance
-console.log(s4counter1.increment());
-console.log(s4counter1.decrement());
-console.log(s4counter1.getValue());
-console.log(s4counter1.reset());
+// console.log(s4counter1.increment());
+// console.log(s4counter1.decrement());
+// console.log(s4counter1.getValue());
+// console.log(s4counter1.reset());
 
 // 2nd counter instance
-console.log(s4counter2.increment());
-console.log(s4counter2.decrement());
-console.log(s4counter2.getValue());
-console.log(s4counter2.reset());
+// console.log(s4counter2.increment());
+// console.log(s4counter2.decrement());
+// console.log(s4counter2.getValue());
+// console.log(s4counter2.reset());
+
+
+
+// ---------------------------STEP 5-------------------------------------------------------------------------------
+
+function s5createCounter(initialValue = 0){
+    let count = initialValue;
+
+    let counter = {
+        __proto__ : counterPrototype,
+        // created private methods to override prototype methods
+        increment() { return count+=2 },
+        decrement() {return count-=1},
+        getValue() {return count},
+        reset() {return count = initialValue}, 
+        // added higher order function 
+        transform(transformFn){
+            return transformFn(count);
+        } 
+    }
+    return counter;
+};
+
+const s5counter1 = s5createCounter(5);
+
+console.log(s5counter1.transform( value =>{
+    return value * 2;
+}))
+
+console.log(s5counter1.transform( value =>{
+    Math.max(value, 0)
+    return value;
+}))
