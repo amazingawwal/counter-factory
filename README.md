c2fb396470279ca2c85eed64350ef241e3cc677e
amazingawwal <sakeebmawwal@gmail.com>
2025-06-12 19:38:35 +0100

    Task 1 and 2  completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# counter-factory
+# Step 1
+## Task 1.1
+### Main Challenge(s)
+- The use of 'this' keyword is not achievable with arrow function when using ES6. Throws error that variable is 'Undefined'
+
+
+# Step 2
+## Task 2.2
+### Why we created Prototype first and not place methods directly in the counter instances
+- We created the Prototype to serve as a template with which we can make us of any time without polluting our code base by making different methods for different counter instance. This makes our code DRY.
+- Also, I believe using the prototype as saves memory space and increases efficiency.
+
---
+++ app.js
@@ -0,0 +1,64 @@
+// -------------------------TASK 1----------------------------------------------------------------
+// This is a counter factory (Constructor ) function that creates counter objects
+function CreateCounter (value){
+    this.value = value
+};
+
+// Creating counter objects with private count value which are instances of the counter factory
+const counter1 = new CreateCounter(5);
+const counter2 = new CreateCounter(10);
+
+// This adds new methods to the count factory function which can be inherited by the counter objects
+CreateCounter.prototype.increment = function (){
+    return this.value++;
+};
+CreateCounter.prototype.getValue = function (){
+    return this.value;
+};
+
+// console.log(`counter1 value: ${counter1.getValue()}`);
+// counter1.increment();
+// console.log(`counter2 value: ${counter2.getValue()}`);
+// console.log(`new counter1 value: ${counter1.getValue()}`);
+
+
+
+// ---------------------------TASK 2-------------------------------------------------------------------------------
+
+CreateCounter.prototype.methods = {
+    increment() {},
+    decrement() {},
+    getValue() {},
+    reset() {}
+}
+
+// console.log(counter1.methods.increment())
+
+const counterPrototype = {
+    increment() { return `Hello`},
+    decrement() {},
+    getValue() {},
+    reset() {}
+}
+
+// ---------------------------TASK 3-------------------------------------------------------------------------------
+
+function createCounter(initialValue = 0){
+    let count = initialValue;
+
+    let counter = {
+        count,
+        __proto__ : counterPrototype    
+    }
+    return counter;
+}
+
+const counter = createCounter(1);
+console.log(counter.count)
+
+const counter3 = createCounter();
+console.log(counter3.count);
+
+
+
+
